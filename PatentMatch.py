ee4ad748387deab0613e11f1bf36df48ca0013aa
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2023-08-04 18:06:04 -0600

    seeing basically random performance

--- PatentMatch.py
+++ PatentMatch.py
@@ -5,8 +5,9 @@ import pandas as pd
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-system_prompt = '''You are given target phrases and need to determine if they are related to the anchor phrase. The phrases come from US Patents. You can answer 'similar', 'not similar', or 'unsure'.'''
+system_prompt = '''You are given target phrases and need to determine if they are related to the anchor phrase. The phrases come from US Patents. You can answer 'similar', 'not similar', or 'unsure'. Tune your responses so they are are equally likely'''
 phrase_map = {'similar':1,'not similar':0, 'unsure':.5}
+round_max_loss = {0:1,.25:.75,.5:.5,.75:.75,1:1}
 df = pd.read_csv("train.csv")
 
 def checkPhrase(one, two):
@@ -15,11 +16,12 @@ def checkPhrase(one, two):
       messages=[
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": f"Anchor: {one}\n Target: {two}"},
-        ]
+        ],
+      temperature=0
     )
 
 def scoreCompletion(completion):
-        return phrase_map[completion.choices[0].message.content]
+        return phrase_map[completion.choices[0].message.content.lower()]
 
 
 def compareScores(completion_score,real_score):
@@ -33,13 +35,16 @@ def compareScores(completion_score,real_score):
 
 count = 0
 loss = 0
-
+max_loss = 0
+print(df.head(6))
 for i in range(len(df)):
     completion = checkPhrase(df.loc[i,'anchor'],df.loc[i,'target'])
     print(completion.choices[0].message.content)
     loss = loss + compareScores(scoreCompletion(completion),df.loc[i,'score'])
+    max_loss = max_loss + round_max_loss[df.loc[i,'score']]
     count = count+1
-    if count>5:
+    if count>50:
         break
 
-print(loss)
+print(1-loss/max_loss)
+print(df.loc[:, 'score'].mean())
